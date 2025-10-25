b79384e9074434a464949a1928d7df0f51b6c83a
ferran.simon <fsc666@gmail.com>
2025-10-26 01:57:30 +0200

    Change backend config

--- backend-afa/config/server.ts
+++ backend-afa/config/server.ts
@@ -3,5 +3,8 @@ export default ({env}) => ({
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS')
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false)
   }
 })
