43f1dc542674d3a5293b8f0ad3b2414854453441
ferran.simon <fsc666@gmail.com>
2025-10-25 20:08:10 +0200

    Added proxy config

--- backend-afa/config/server.ts
+++ backend-afa/config/server.ts
@@ -1,7 +1,8 @@
-export default ({ env }) => ({
+export default ({env}) => ({
+  proxy: true,
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+    keys: env.array('APP_KEYS')
+  }
+})
